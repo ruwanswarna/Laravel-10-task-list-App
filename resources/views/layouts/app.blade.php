5987c94a4a881a58c276cb40dbfa176d5ab83006
ruwanswarna <ruwanswarna@gmail.com>
2024-09-06 16:35:26 +0530

    created layout file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel 10 task list App</title>
+    <title>@yield('title')</title>
+    <div>@yield('content')</div>
 </head>
 <body>
     
